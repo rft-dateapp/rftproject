4e6afcbba39cc26b72f514ed3bc2f5ab8ac44da4
Baxi0116 <gergely0116@freemail.hu>
2017-10-24 15:26:12 +0200

    started working on android app

---
+++ rft-kocsmapp/app/src/main/java/com/baxi/android/rft_kocsmapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.baxi.android.rft_kocsmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showPubs(View view){
+        //TODO
+    }
+
+    public void showMap(View view){
+        //TODO
+    }
+
+    public void suggestOne(View view){
+        //TODO
+    }
+
+    public void exitApp(View view){
+        //TODO
+    }
+
+    public void facebookLogin(View view){
+        //TODO
+    }
+}
