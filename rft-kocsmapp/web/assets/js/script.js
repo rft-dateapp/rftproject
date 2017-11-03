8f2d3404bce12542b1d6cf6ad11e422d0569f955
collion <bmdenis95@gmail.com>
2017-11-03 20:04:20 +0100

    Fix Navigation Bar

---
+++ rft-kocsmapp/web/assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var stickyNavTop = $('nav').offset().top;
+    
+      var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+    
+        if (scrollTop > stickyNavTop) { 
+          $('nav').addClass('fixed');
+        } else {
+          $('nav').removeClass('fixed'); 
+        }
+      };
+    
+      stickyNav();
+    
+      $(window).scroll(function() {
+        stickyNav();
+      });
+
+});
