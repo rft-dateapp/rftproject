5ddae69ceb6d2ae6356e13b43e0ad64772b52aa7
collion <bmdenis95@gmail.com>
2017-11-06 23:32:03 +0100

    Add geolocation

--- rft-kocsmapp/web/assets/js/script.js
+++ rft-kocsmapp/web/assets/js/script.js
@@ -1,21 +1,25 @@
-$(document).ready(function(){
-
-    var stickyNavTop = $('nav').offset().top;
+(function(){
     
-      var stickyNav = function(){
-        var scrollTop = $(window).scrollTop();
-    
-        if (scrollTop > stickyNavTop) { 
-          $('nav').addClass('fixed');
-        } else {
-          $('nav').removeClass('fixed'); 
-        }
-      };
-    
-      stickyNav();
-    
-      $(window).scroll(function() {
+    $(document).ready(function(){
+        
+        var stickyNavTop = $('nav').offset().top;
+        
+        var stickyNav = function(){
+            var scrollTop = $(window).scrollTop();
+            
+            if (scrollTop > stickyNavTop) { 
+                $('nav').addClass('fixed');
+            } else {
+                $('nav').removeClass('fixed'); 
+            }
+        };
+        
         stickyNav();
-      });
-
-});
+        
+        $(window).scroll(function() {
+            stickyNav();
+        });
+        
+    });
+    
+}());
