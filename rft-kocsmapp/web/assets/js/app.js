fb64a4b16db67e38395828ad3560933855eba0b9
collion <bmdenis95@gmail.com>
2017-11-27 12:52:21 +0100

    Add routing

--- rft-kocsmapp/web/assets/js/app.js
+++ rft-kocsmapp/web/assets/js/app.js
@@ -1,15 +1,16 @@
 var app = angular.module('pubNFun', ['easyFacebook','ngRoute'])
-.config(function($routeProvider, $locationProvider){
+.config(function($routeProvider){
       $routeProvider
       .when("/home", {
-        templateUrl: "assets/templates/home.html",
+        templateUrl: "assets/templates/pages/home/index.html",
         controller: "homeController"
-      })
+      }) 
       .when("/pubs", {
-        templateUrl: "assets/templates/pubs.html",
+        templateUrl: "assets/templates/pages/pubs/index.html",
         controller: "pubsController"
       })
 })
+
 
 .controller("homeController", function($scope){
  })
