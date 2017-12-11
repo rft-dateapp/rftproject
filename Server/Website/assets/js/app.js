b8af3cf897a6bab40a21483a68e6e7d1ba0293a3
collion <bmdenis95@gmail.com>
2017-12-11 20:25:52 +0100

    Add bugfix

--- Server/Website/assets/js/app.js
+++ Server/Website/assets/js/app.js
@@ -126,11 +126,11 @@ var app = angular.module('pubNFun', ['easyFacebook','ngRoute'])
     
     .service('pubService', function($http){
         this.getPubs = function(){
-            return $http.get('http://pubnfun.azurewebsites.net/PubnFunCore.svc/GetAllPub');
+            return $http.get('http://rftpubnfun.azurewebsites.net/PubnFunCore.svc/GetAllPub');
         };
         
         this.getOpinionsByPubID = function(pubID){
-            return $http.get('http://pubnfun.azurewebsites.net/PubnFunCore.svc/GetAllOpinionAboutPubByID/' + pubID);
+            return $http.get('http://rftpubnfun.azurewebsites.net/PubnFunCore.svc/GetAllOpinionAboutPubByID/' + pubID);
         };
     })
     
